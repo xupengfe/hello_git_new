7631e13c073d67c846c366bf9114598081dfb27c
Pengfei Xu <pengfei.xu@intel.com>
2022-10-20 15:28:02 +0800

    set_area.c: improve the set_area syscall
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- syscall/set_area.c
+++ syscall/set_area.c
@@ -7,11 +7,15 @@
 
 int main() {
 	unsigned long addr = 0;
+	struct user_desc u_info;
 	int ret = 0;
 
-	ret = syscall(SYS_set_thread_area, addr);
+	u_info.entry_number = -1; 
+	//ret = syscall(SYS_set_thread_area, addr);
+	ret = syscall(SYS_set_thread_area, &u_info);
 
 	printf("ret:%d, addr:%lx, errno:%d\n", ret, addr, errno);
+	printf("entry_number is %d\n",u_info.entry_number);
 
 	return 0;
 
