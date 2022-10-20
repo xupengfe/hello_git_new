f09e76e7f1b2a8d151daf0ea4fbd4058d127bb93
Pengfei Xu <pengfei.xu@intel.com>
2022-10-20 16:46:41 +0800

    set_area.c: improve the code input
    As linux kernel commit:
    commit 3669ef9fa7d35f573ec9c0e0341b29251c2734a7
    Author: Andy Lutomirski <luto@amacapital.net>
    Date:   Thu Jan 22 11:27:59 2015 -0800
    
        x86, tls: Interpret an all-zero struct user_desc as "no segment"
    
        The Witcher 2 did something like this to allocate a TLS segment index:
    
                struct user_desc u_info;
                bzero(&u_info, sizeof(u_info));
                u_info.entry_number = (uint32_t)-1;
    
                syscall(SYS_set_thread_area, &u_info);
    
        Strictly speaking, this code was never correct.  It should have set
        read_exec_only and seg_not_present to 1 to indicate that it wanted
        to find a free slot without putting anything there, or it should
        have put something sensible in the TLS slot if it wanted to allocate
        a TLS entry for real.  The actual effect of this code was to
        allocate a bogus segment that could be used to exploit espfix.
    ...
    
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- syscall/set_area.c
+++ syscall/set_area.c
@@ -4,13 +4,18 @@
 #include <sys/types.h>
 #include <asm/ldt.h>
 #include <errno.h>
+#include <stdint.h>
 
 int main() {
 	unsigned long addr = 0;
 	struct user_desc u_info;
 	int ret = 0;
 
-	u_info.entry_number = -1; 
+	u_info.entry_number = (uint32_t)-1;
+	u_info.read_exec_only = 0x1;
+	u_info.seg_not_present = 0x1;
+	printf("read_exec_only:%d, seg_not_present:%d\n", u_info.read_exec_only,
+		u_info.seg_not_present);
 	//ret = syscall(SYS_set_thread_area, addr);
 	ret = syscall(SYS_set_thread_area, &u_info);
 
