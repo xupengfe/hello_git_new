a371c258e2c7ed390bcbd30cd8ce7eb1e1083e57
Pengfei Xu <pengfei.xu@intel.com>
2022-12-06 17:09:13 +0800

    bridge.sh: add more prints for it
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- bridge.sh
+++ bridge.sh
@@ -2,13 +2,32 @@
 
 eth="enp0s20f0u1"
 
+echo "nmcli connection delete $eth"
 nmcli connection delete $eth
+
+echo "nmcli connection delete $eth"
 nmcli connection delete $eth
+
+echo "nmcli connection delete virbr0"
 nmcli connection delete virbr0
+
+echo "nmcli connection delete virbr0"
 nmcli connection delete virbr0
+
+echo "nmcli connection add type ethernet con-name $eth ifname $eth"
 nmcli connection add type ethernet con-name $eth ifname $eth
+
+echo "nmcli connection add type bridge con-name virbr0 ifname virbr0"
 nmcli connection add type bridge con-name virbr0 ifname virbr0
+
+echo "nmcli connection modify virbr0 stp no"
 nmcli connection modify virbr0 stp no
+
+echo "nmcli connection modify $eth master virbr0"
 nmcli connection modify $eth master virbr0
+
+echo "nmcli connection modify virbr0 ipv4.method auto"
 nmcli connection modify virbr0 ipv4.method auto
+
+echo "nmcli connection up virbr0"
 nmcli connection up virbr0
