148eb22db9456d976cb76712ee47d4cb9058c7de
Pengfei Xu <pengfei.xu@intel.com>
2023-04-07 11:29:28 +0800

    hello_sys.c: improve hello_sys.c
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- hello_sys.c
+++ hello_sys.c
@@ -2,10 +2,8 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 
-int main(void)
+void main(void)
 {
     char *msg = "Hello world\n";
     syscall(SYS_write, 1, msg, strlen(msg));
-
-    return 0;
 }
