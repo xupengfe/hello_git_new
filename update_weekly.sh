178bbfca8c57f8d09b0c290d8c4b5ad685a34140
Pengfei Xu <pengfei.xu@intel.com>
2024-04-01 10:33:40 +0800

    update_weekly: update DDT package to haydn and artifactory script
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

---
+++ update_weekly.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FILE=$1
+# It's from Idisd token: https://ubit-artifactory-sh.intel.com/ui/repos/tree/General/clkv-infra-sh-local
+# Choose Idisd -> Set me up -> Generic -> Generate API Key
+PASSWD=""
+
+if [[ "$FILE" == *"gz" ]]; then
+  echo "scp $FILE root@haydn.sh.intel.com:/root/Data/DDT/packages/weekly/centos/9/"
+  scp "$FILE" root@haydn.sh.intel.com:/root/Data/DDT/packages/weekly/centos/9/
+else
+  echo "FILE:$FILE is not a gz file, will not upload into haydn.sh.intel.com:/root/Data/DDT/packages/weekly/centos/9/"
+
+fi
+
+  echo "curl -uxupengfe:$PASSWD -T $FILE -O https://ubit-artifactory-sh.intel.com/artifactory/clkv-infra-sh-local/builds/weekly/centos/9/$FILE"
+  curl -uxupengfe:"$PASSWD" -T "$FILE" -O "https://ubit-artifactory-sh.intel.com/artifactory/clkv-infra-sh-local/builds/weekly/centos/9/$FILE"
